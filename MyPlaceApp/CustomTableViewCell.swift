8113e63462c1c22b6ab4eaa8f1f4f9407751903c
Vadim Gamzaev <Vadosbox@gmail.com>
2019-06-26 18:51:34 +0300

    Custom Table View Cell

---
+++ MyPlaceApp/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaceApp
+//
+//  Created by Вадим Гамзаев on 26/06/2019.
+//  Copyright © 2019 Вадим Гамзаев. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
--- MyPlaceApp/MainTableView.swift
+++ MyPlaceApp/MainTableView.swift
@@ -28,15 +28,15 @@ class MainTableView: UITableViewController {
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
 
-        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.nameLabel?.text = restaurantNames[indexPath.row]
         
-        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageOfPlace?.image = UIImage(named: restaurantNames[indexPath.row])
         
-        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2 //делаем картинки круглыми
+        cell.imageOfPlace?.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2 //делаем картинки круглыми
         
-        cell.imageView?.clipsToBounds = true
+        cell.imageOfPlace?.clipsToBounds = true
 
         return cell
     }
